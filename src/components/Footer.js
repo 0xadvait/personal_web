d10a15496b58c056aacc61e22daea26520cd6b12
Advait Jayant <advaitjk@gmail.com>
2026-05-14 00:32:08 +0100

    Remove footer font credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,6 @@ export default function Footer() {
             advait.tech
           </a>
           <span>London</span>
-          <span className="hidden sm:inline">Iowan + Geist</span>
         </div>
       </div>
     </footer>
